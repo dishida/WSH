6376525c10944c7310f1a14cb77d9dce28c61e80
dishida <clishicla+github@gmail.com>
2012-12-11 13:26:59 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Windows Script Host
+===========
+
+WSH for JScript
---
+++ ie.js
@@ -0,0 +1,52 @@
+var ie = function() {
+    var _obj, _doc;
+
+    return {
+        open: function(url, visible) {
+            _obj = new ActiveXObject('InternetExplorer.Application');
+            _obj.Visible = visible !== undefined ? visible : true;
+            this.view(url);
+        },
+        view: function(url) {
+            _obj.Navigate(url !== undefined || url != '' ? url : 'about:blank');
+            this.wait();
+            _doc = _obj.document;
+        },
+        wait: function() {
+			var start_date = new Date();
+            while (_obj.Busy || _obj.ReadyState != 4) {
+                if (typeof WScript === 'object') {
+                    WScript.Sleep(500);
+                }
+				if (new Date() - start_date >= 90 * 1000) {
+					break;
+				}
+            }
+        },
+        close: function() {
+            _obj.Quit();
+        },
+        getId: function(id) {
+            return _doc.getElementById(id);
+        },
+        getClass: function(cName) {
+            try {
+                return _doc.getElementsByClassName(cName);
+            } catch (e) {
+                for (var i = 0, j = 0, ret = [], tags = _doc.getElementsByTagName('*'); tag = tags[i++];) {
+                    if (tag.className == cName) {
+                        ret[j++] = tag;
+                    }
+                }
+                return ret;
+            }
+        },
+        getTag: function(tName) {
+            return _doc.getElementsByTagName(tName);
+        },
+        getName: function(Name) {
+            return _doc.getElementsByName(Name);
+        }
+    };
+}();
+
